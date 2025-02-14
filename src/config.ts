2548eec0f7b5c80ed05fd2bcff0270db243d569b
shohjahon1code <suyunovshohjahon08@mail.ru>
2025-02-14 15:30:04 +0500

    fix: zod validator

--- src/config.ts
+++ src/config.ts
@@ -12,10 +12,10 @@ const configSchema = z.object({
   UPLOAD_DIR: z.string().default('uploads'),
   MINIO_ENDPOINT: z.string().default('s3.glmv.dev'),
   MINIO_PORT: z.string().default('443'),
-  MINIO_USE_SSL: z.string().default('true'),
-  MINIO_ACCESS_KEY: z.string().default('bi9PoWQzPZaKUY0Mnt1I'),
-  MINIO_SECRET_KEY: z.string().default('qYtmSaRA5wd9GO3lylp9HpthzPnLjULE3ImTTc9D'),
-  MINIO_BUCKET_NAME: z.string().default('uway-storage'),
+  MINIO_USE_SSL: z.string(),
+  MINIO_ACCESS_KEY: z.string(),
+  MINIO_SECRET_KEY: z.string(),
+  MINIO_BUCKET_NAME: z.string(),
 });
 
 export const config = configSchema.parse(process.env);
